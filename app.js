5d8f69bda2bede4ce3818e8e48794350c463668b
cabash1960 <blessingxin1960@gmail.com>
2025-09-09 03:50:17 +0100

    message

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-"use strict";
+import { apiKey } from "./config.js";
 
+("use strict");
 const modal = document.querySelector("#modal");
 const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const inputQuote = document.querySelector(".input-quote");
+const loveText = document.querySelector(".love-text");
 
+async function getQuote() {
+  const url = "https://love-quote.p.rapidapi.com/lovequote";
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": apiKey,
+      "x-rapidapi-host": "love-quote.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    loveText.textContent = result.quote;
+    loveText.innerHTML = result.quote
+      .replace(/, /g, ",<br>") // break after commas
+      .replace(/love/gi, "<span>love</span>"); // highlight 'love'
+
+    console.log(result);
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    loveText.textContent = "An error occurred while fetching the quote.";
+  }
+}
+
+getQuote();
 openBtn.addEventListener("click", function () {
   modal.showModal();
 });
@@ -21,4 +49,5 @@ closeBtn.addEventListener("click", function (e) {
     );
   }
   inputQuote.value = "";
+  getQuote();
 });
---
+++ config.js
@@ -0,0 +1,3 @@
+const apiKey = "ca717cdf0bmshd7a124a712b1167p1897e1jsnf0f9a06af45f";
+
+export { apiKey };
